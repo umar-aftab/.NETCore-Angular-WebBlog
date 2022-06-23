8d9b04d58d72a854441ec10a6416d03308678e2f
umar-aftab <umaraftabqamar@gmail.com>
2022-06-23 07:50:21 -0600

    Added Tken Service, PhotoService which connected to Cloudinary

--- BackEnd/BlogLab/BlogLab.Web/Startup.cs
+++ BackEnd/BlogLab/BlogLab.Web/Startup.cs
@@ -1,3 +1,4 @@
+using BlogLab.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace BlogLab.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
